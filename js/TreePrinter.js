d67c1590bcc96f20bcd8357bb961c77b9f80a465
aleksapetrovic2907 <aleksa.petrovic2907@gmail.com>
2025-01-29 17:46:32 +0100

    implement TreePrinter that can print BSTs in a nice format

---
+++ js/TreePrinter.js
@@ -0,0 +1,14 @@
+export class TreePrinter {
+  static print(node, prefix = "", isLeft = true) {
+    if (node === null) {
+      return;
+    }
+    if (node.right !== null) {
+      this.print(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+    }
+    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.value}`);
+    if (node.left !== null) {
+      this.print(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+    }
+  }
+}
